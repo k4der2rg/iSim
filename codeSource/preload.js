1f88b7b58bbe97f682ac655e4e95e21d5b57d7b7
Abdelkader <fa_bouregag@esi.dz>
2018-02-04 02:25:44 +0100

    Optimazing loading, WelcomePage , SEO ...

---
+++ codeSource/preload.js
@@ -0,0 +1,31 @@
+function preload(){
+	
+	images = new Array();
+	for (var i = 0; i<23; i++) {
+		images[i]= new Image();	
+	}	
+	images[0].src="./img/HEX/HEX.png";
+	images[1].src="./img/HEX/out-7seg-0.png";
+	images[2].src="./img/HEX/out-7seg-1.png";
+	images[3].src="./img/HEX/out-7seg-2.png";
+	images[4].src="./img/HEX/out-7seg-3.png";
+	images[5].src="./img/HEX/out-7seg-4.png";
+	images[6].src="./img/HEX/out-7seg-5.png";
+	images[7].src="./img/HEX/out-7seg-6.png";
+	images[8].src="./img/HEX/out-7seg-7.png";
+	images[9].src="./img/HEX/out-7seg-8.png";
+	images[10].src="./img/HEX/out-7seg-9.png";
+	images[11].src="./img/HEX/out-7seg-10.png";
+	images[12].src="./img/HEX/out-7seg-11.png";
+	images[13].src="./img/HEX/out-7seg-12.png";
+	images[14].src="./img/HEX/out-7seg-13.png";
+	images[15].src="./img/HEX/out-7seg-14.png";
+	images[16].src="./img/HEX/out-7seg-15.png";
+	images[17].src="./img/clock-on.png";
+	images[18].src="./img/out-on.png";
+	images[19].src="./img/out-off.png";
+	images[20].src="./img/in-on.png";
+	images[21].src="./img/off-on.png";
+	images[22].src="./img/chrono.png";
+
+}
